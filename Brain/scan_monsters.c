5e88af0179875350df9e031f7af6a7528ee7e601
Bredian <sasha.kry@gmail.com>
2016-12-10 14:25:57 +0300

    Update scan_monsters.c

--- Brain/scan_monsters.c
+++ Brain/scan_monsters.c
@@ -8,5 +8,16 @@ typedef struct  monster{
 } monster;
 
 void scan_for_monstrers(monster * monster_db,char* map){
-	
+	int i,j;
+	for(i=0;i<MAX_X;i++){
+		for(j=0;j<MAX_Y;j++){
+			if(map[i][j]=='@'){
+				monster_db=realloc(monster_db,sizeof(monster));
+				monster_db->x=i;
+				monster_db->y=j;
+				monster_db->alive=1;
+			}
+		}
+	}
+	return;
 }
