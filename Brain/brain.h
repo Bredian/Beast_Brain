73b01e00cc78549026863bc387d78e2efbbae824
fedosgad <fedosgad@gmail.com>
2016-12-10 14:30:02 +0300

    wrote header brain.h

--- Brain/brain.h
+++ Brain/brain.h
@@ -1,3 +1,5 @@
+/*structs*/
+
 struct _point {
 	int x;
 	int y;
@@ -12,3 +14,26 @@ struct _monster {
 }
 
 typedef _monster monster;
+
+struct _treasure {
+	int x;
+	int y;
+	int exist;
+}
+
+typedef _treasure treasure;
+
+
+/*functions*/
+
+void scan_for_treasures(treasure* treasure_db, char* map);
+void scan_for_monsters(monster* monster_db, char* map);
+void refresh_treasure_db(treasure* treasure_db, char* map);
+void refresh_monster_db(monster* monster_db, char* map);
+
+treasure* find_nearest_treasure(point from, treasure* treasure_db);	//decartian distance
+
+int wave_scan(point from, int range, char* map);	//returns number of treasures in range
+int wave_scan_to_point(point from, point to, char* map);	//returns number of steps
+
+void monster_update(char* map; monster* monster_db);	//idk what's it for
