9ebe0ca34717d00abdef5c2639dcfcfd26e4123c
Bredian <sasha.kry@gmail.com>
2016-12-10 20:25:35 +0300

    Update and rename scan_for_treasures.c to treasures_db.c

--- Brain/treasures_db.c
+++ Brain/treasures_db.c
@@ -15,3 +15,10 @@ void scan_for_treasures(treasure* treasure_db, char (*map)[MAX_Y][MAX_X]){
     }
     return;
 }
+
+void refresh_treasure_db(treasure* treasure_db, char (*map)[MAX_Y][MAX_X]){
+    int i;
+    for(i=0;i<number_of_treasures;i++)
+        if((*map)[treasure_db[i].y][treasure_db[i].x]!='*') treasure_db[i].exist=0;
+	return;
+}
