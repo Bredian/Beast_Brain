b26c420d7b165fb6b7281a7ffd61f62864f1c198
Bredian <sasha.kry@gmail.com>
2016-12-11 00:05:35 +0300

    Update brain.c

--- Brain/brain.c
+++ Brain/brain.c
@@ -2,7 +2,9 @@
 #include "brain.h"
 
 void init(){
-	scan_for_treasures()	
+	mem
+	scan_for_treasures(treasure_db,map);
+	scan_for_monsters(monster_db,map);	
 }
 
 void do_next_step(int cur_x, int cur_y, int *command) {
