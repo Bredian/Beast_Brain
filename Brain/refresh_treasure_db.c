c543cc77bf460399191daa9b287edcaa7335b130
Bredian <sasha.kry@gmail.com>
2016-12-10 20:27:06 +0300

    Delete refresh_treasure_db.c

--- Brain/refresh_treasure_db.c
+++
@@ -1,7 +0,0 @@
-#include <brain.h>
-void refresh_treasure_db(treasure* treasure_db, char (*map)[MAX_Y][MAX_X]){
-    int i;
-    for(i=0;i<number_of_treasures;i++)
-        if((*map)[treasure_db[i].y][treasure_db[i].x]!='*') treasure_db[i].exist=0;
-	return;
-}
