aa54828d1eb8abb4d8e46a572e4456beb59c1979
Bredian <sasha.kry@gmail.com>
2016-12-10 14:07:05 +0300

    Update scan_treasures.c

--- scan_treasures.c
+++ scan_treasures.c
@@ -7,11 +7,11 @@ typedef struct treasue{
 	int existance;
 } treasure;
 
-void scan_for_treasures(treasure* treasure_db, char** map){
+void scan_for_treasures(treasure* treasure_db, char* map){
 	int i,j;
 	for(i=0;i<MAX_X;i++){
 		for(j=0;j<MAX_Y;j++){
-			if(map[i][j]=='*'){
+			if(map[i][j]=='*'){		
 				treasure_db=realloc(sizeof(treasue));
 				treasure_db.x=j;
 				treasure_db.y=i;
