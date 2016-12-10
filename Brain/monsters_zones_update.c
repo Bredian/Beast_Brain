909f1a1ce2121b6fe517802d4cf27e3b881fc65d
Bredian <sasha.kry@gmail.com>
2016-12-10 23:40:10 +0300

    Update monsters_zones_update.c

--- Brain/monsters_zones_update.c
+++ Brain/monsters_zones_update.c
@@ -1,13 +1,13 @@
 #include "brain.h"
 void monster_zones_update(char (*monster_map)[max_y][max_x], monster* monster_db){
 	int i;
-	point *monster_place=malloc(number_of_monsters*sizeof(point));
+	point monster_place;
 	for(i = 0; i < number_of_monsters; i++){
 		if(monster_db[i].alive == 0) 
 			continue;
-		monster_place[i].x = monster_db[i].x;
-		monster_place[i].y = monster_db[i].y;
-		wave_scan(point monster_place, 5, (*monster_map)[max_y][max_x]);		
+		monster_place.x = monster_db[i].x;
+		monster_place.y = monster_db[i].y;
+		wave_scan(monster_place, 5, (*monster_map)[max_y][max_x]);		
 	}
 	return;
 }
