a90be5bd636a82a3265fa1243a0a7425d560fb08
fedosgad <fedosgad@gmail.com>
2016-12-10 23:19:12 +0300

    test for branching

---
+++ Brain/pathfind.c
@@ -0,0 +1,10 @@
+
+#include "brain.c"
+
+path trace(point from, char (*map)[max_y][max_x]) {
+	
+}
+
+path trace_with_monsters(point from, char (*monster_map)[max_y][max_x], char (*map)[max_y][max_x]) {
+	
+}
