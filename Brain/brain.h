869a0031635798ba20ce953dde9080de59c831bf
Bredian <sasha.kry@gmail.com>
2016-12-10 19:56:52 +0300

    Update brain.h

--- Brain/brain.h
+++ Brain/brain.h
@@ -1,27 +1,27 @@
 /*structs*/
 
 struct _point {
-	int x;
-	int y;
-}
+    int x;
+    int y;
+};
 
-typedef _point point;
+typedef struct _point point;
 
 struct _monster {
-	int x;
-	int y;
-	int alive;
-}
+    int x;
+    int y;
+    int alive;
+};
 
-typedef _monster monster;
+typedef struct _monster monster;
 
 struct _treasure {
-	int x;
-	int y;
-	int exist;
-}
+    int x;
+    int y;
+    int exist;
+};
 
-typedef _treasure treasure;
+typedef struct _treasure treasure;
 
 /*variables*/
 
@@ -41,3 +41,19 @@ int wave_scan(point from, int dist, char (*map)[MAX_Y][MAX_X]);	//returns number
 int wave_scan_to_point(point from, point to, char (*map)[MAX_Y][MAX_X]);	//returns number of steps
 
 void monster_update(char (*map)[MAX_Y][MAX_X], monster* monster_db);	//idk what's it for
+void scan_for_treasures(treasure* treasure_db, char (*map)[MAX_Y][MAX_X]){
+    int i,j;
+    number_of_treasures=0;
+    for(i=0;i<MAX_Y;i++){
+        for(j=0;j<MAX_X;j++){
+            if((*map)[i][j]=='*'){
+                number_of_treasures++;
+                treasure_db=realloc(treasure_db,number_of_treasures*sizeof(treasure));
+                treasure_db->x=j;
+                treasure_db->y=i;
+                treasure_db->exist=1;
+            }
+        }
+    }
+    return;
+}
