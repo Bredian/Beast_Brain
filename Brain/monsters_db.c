98fc9c30cd8a0315b6e6e5428ad08ba99951d5ee
fedosgad <fedosgad@gmail.com>
2016-12-10 21:03:54 +0300

    monsters functions remake

--- Brain/monsters_db.c
+++ Brain/monsters_db.c
@@ -1,37 +1,38 @@
 #include <brain.h>
-void scan_for_monstrers(monster * monster_db,char (*map)[MAX_Y][MAX_X]){
-    int i,j;
-    number_of_monsters=0;
-    for(i=0;i<MAX_Y;i++){
-        for(j=0;j<MAX_X;j++){
-            if((*map)[i][j]=='@'){
-                number_of_monsters++;
-                monster_db=realloc(monster_db,number_of_monsters*sizeof(monster));
-                monster_db->x=j;
-                monster_db->y=i;
-                monster_db->alive=1;
-            }
-        }
-    }
-    return;
+
+void scan_for_monsters(monster* monster_db, char (*map)[MAX_Y][MAX_X]){
+	int i, j;
+	number_of_monsters = 0;
+	for(i = 0; i < MAX_Y; i++) {
+		for(j = 0; j < MAX_X; j++) {
+			if( (*map)[i][j] == '@' ){
+				number_of_monsters++;
+				monster_db = realloc(monster_db, number_of_monsters*sizeof(monster));
+				monster_db[number_of_monsters].y = i;
+				monster_db[number_of_monsters].x = j;
+				monster_db[number_of_monsters].alive = 1;
+			}
+		}
+	}
+	return;
 }
 
-void refresh_monster_db(monster * monster_db,char (*map)[MAX_Y][MAX_X]){
-    int i;
-    for(i=0;i<number_of_monsters;i++){
-        if((*map)[monster_db[i].y][monster_db[i].x]=='@')
-            monster_db[i].alive=1;
-        if((*map)[monster_db[i].y+1][monster_db[i].x]=='@')
-            monster_db[i].y=monster_db[i].y+1;
-        if((*map)[monster_db[i].y-1][monster_db[i].x]=='@')
-            monster_db[i].y=monster_db[i].y-1;
-        if((*map)[monster_db[i].y][monster_db[i].x+1]=='@')
-            monster_db[i].x=monster_db[i].x+1;
-        if((*map)[monster_db[i].y][monster_db[i].x-1]=='@')
-            monster_db[i].x=monster_db[i].x-1;
-        else
-            monster_db[i].alive=0;
-    }
+void refresh_monster_db(monster* monster_db, char (*map)[MAX_Y][MAX_X]) {
+	int i, j, x, y;
+	int dx[5] = {0, -1, 0, 1, 0};	//step from current position to
+	int dy[5] = {0, 0, 1, 0, -1};	//nowhere, left, up, right, down
+
+	for(i = 0; i < number_of_monsters; i++) {
+		if(monster_db[i].alive == 0) continue;
+ 		for(j = 0; j < 5; j++) {
+			if( (*map)[monster_db[i].y + dy[j]][monster_db[i].x + dx[j]] == '@' ) {
+				monster_db[i].x = monster_db[i].x + dx[j];
+				monster_db[i].y = monster_db[i].y + dy[j];
+				break;
+			}
+			monster_db[i].alive = 0;
+		}
+	}
 	return;
 }
 
