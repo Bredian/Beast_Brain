218869a5d3791f7d0e00709cdba64740e03e1852
Bredian <sasha.kry@gmail.com>
2016-12-11 00:10:17 +0300

    Update brain.c

--- Brain/brain.c
+++ Brain/brain.c
@@ -1,8 +1,10 @@
-
+#include <stdlin.h>
 #include "brain.h"
 
 void init(){
-	mem
+	memcpy(monster_map, map, max_y * max_x * sizeof(char));
+	treasure_db = NULL;
+	monster_db = NULL;
 	scan_for_treasures(treasure_db,map);
 	scan_for_monsters(monster_db,map);	
 }
