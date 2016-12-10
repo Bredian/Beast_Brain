08f5ed0cd457a7f9f67b57e5a4b5723538db045d
Bredian <sasha.kry@gmail.com>
2016-12-10 20:07:00 +0300

    Update refresh_monster_db.c

--- Brain/refresh_monster_db.c
+++ Brain/refresh_monster_db.c
@@ -1,17 +1,19 @@
 #include <brain.h>
-void refresh_monster_db(monster * monster_db,char (*map)[MAX_Y][MAP_X]){
-	for(i=0;i<number_of_monster){
-		if(map[monster_db[i]->y][monster_db[i]->x]=='@')
-			monster_db[i]->alive=1;
-		if(map[monster_db[i]->y+1][monster_db[i]->x]=='@')
-			monster_db[i]->y=monster_db[i]->y+1;
-		if(map[monster_db[i]->y-1][monster_db[i]->x]=='@') 
-			monster_db[i]->y=monster_db[i]->y-1;
-		if(map[monster_db[i]->y][monster_db[i]->x+1]=='@') 
-			monster_db[i]->x=monster_db[i]->x+1;
-		if(map[monster_db[i]->y][monster_db[i]->x-1]=='@') 
-			monster_db[i]->x=monster_db[i]->x-1;
-		else
-			monster_db[i]->alive=0;
-	}
+void refresh_monster_db(monster * monster_db,char (*map)[MAX_Y][MAX_X]){
+    int i;
+    for(i=0;i<number_of_monsters;i++){
+        if((*map)[monster_db[i].y][monster_db[i].x]=='@')
+            monster_db[i].alive=1;
+        if((*map)[monster_db[i].y+1][monster_db[i].x]=='@')
+            monster_db[i].y=monster_db[i].y+1;
+        if((*map)[monster_db[i].y-1][monster_db[i].x]=='@')
+            monster_db[i].y=monster_db[i].y-1;
+        if((*map)[monster_db[i].y][monster_db[i].x+1]=='@')
+            monster_db[i].x=monster_db[i].x+1;
+        if((*map)[monster_db[i].y][monster_db[i].x-1]=='@')
+            monster_db[i].x=monster_db[i].x-1;
+        else
+            monster_db[i].alive=0;
+    }
+	return;
 }
