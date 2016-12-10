34d959b9d2d2a5df4702e74b368b23f3ce3f3a6e
Bredian <sasha.kry@gmail.com>
2016-12-10 13:55:16 +0300

    Update scan_treasures.c

--- scan_treasures.c
+++ scan_treasures.c
@@ -8,7 +8,10 @@ typedef struct treasue{
 } treasure;
 
 void scan_for_treasures(treasure* treasure_db, char** map){
+	treasure_db=malloc(20*sizeof(treasue))
 	int i,j;
 	for(i=0;i<MAP_X;i++){
-		for()
+		for(j=0;j<MAP_Y;j++){
+			if(map[i][j]=='*'){
+				
 }
