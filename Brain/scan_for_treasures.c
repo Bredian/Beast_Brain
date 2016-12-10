8ff40ec1e859a8a0258d0f9f5d1b3fe9ea43a06f
Bredian <sasha.kry@gmail.com>
2016-12-10 14:35:32 +0300

    Update and rename scan_treasures.c to scan_for_treasures.c

--- Brain/scan_for_treasures.c
+++ Brain/scan_for_treasures.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct treasure{
-    int x;
-    int y;
-    int exist;
-} treasure;
-
+#include <brain.h>
 void scan_for_treasures(treasure* treasure_db, char* map){
     int i,j;
     for(i=0;i<MAX_X;i++){
         for(j=0;j<MAX_Y;j++){
             if(map[i][j]=='*'){
+                number_of_treasures++;
                 treasure_db=realloc(treasure_db,sizeof(treasure));
                 treasure_db->x=i;
                 treasure_db->y=j;
