1898453ecd5038fa5f57c24e76a81f546247d171
Bredian <sasha.kry@gmail.com>
2016-12-10 23:20:10 +0300

    Update monsters_zones_update.c

--- Brain/monsters_zones_update.c
+++ Brain/monsters_zones_update.c
@@ -1,2 +1,4 @@
 #include "brain.h"
-void monster_zones_update(char (*monster_map)[max_y][max_x], monster* monster_db);
+void monster_zones_update(char (*monster_map)[max_y][max_x], monster* monster_db){
+	
+}
