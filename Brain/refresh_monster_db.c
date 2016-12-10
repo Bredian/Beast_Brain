f11f19c7a6e6573539a9b6ee004867bbe4fb884c
Bredian <sasha.kry@gmail.com>
2016-12-10 16:17:51 +0300

    Update refresh_monster_db.c

--- Brain/refresh_monster_db.c
+++ Brain/refresh_monster_db.c
@@ -2,5 +2,11 @@
 #include <stdlib.h>
 #include <brain.h>
 void refresh_monster_db(monster * monster_db,char (*map)[MAX_Y][MAP_X]){
-	if(map)
+	for(i=0;i<number_of_monster)
+	if(map[monster_db[i]->y][monster_db->x]=='@') monster_db[i]->alive=1;
+	if(map[monster_db[i]->y+1][monster_db->x]=='@') monster_db[i]->y=monster_db[i]->y+1;
+	if(map[monster_db[i]->y-1][monster_db->x]=='@') monster_db[i]->y=monster_db[i]->y-1;
+	if(map[monster_db[i]->y][monster_db->x+1]=='@') monster_db[i]->x=monster_db[i]->x+1;
+	if(map[monster_db[i]->y][monster_db->x-1]=='@') monster_db[i]->x=monster_db[i]->x-1;
+	else monster_db[i]->alive=0;
 }
