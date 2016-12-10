451bc6ce0b50265a66fbe072cdf9fdf5ea940f3a
Bredian <sasha.kry@gmail.com>
2016-12-10 20:05:43 +0300

    Update refresh_treasure_db.c

--- Brain/refresh_treasure_db.c
+++ Brain/refresh_treasure_db.c
@@ -1,6 +1,7 @@
 #include <brain.h>
-void refresh_treasure_db(treasure* treasure_db, char (*map)[MAP_Y][MAP_X]){
-	int i;
-	for(i=0;i<number_of_treasures;i++)
-		if(map[treausre_db[i]->y][treasure_db->x]!='*') treasure_db[i]->exist=0;
+void refresh_treasure_db(treasure* treasure_db, char (*map)[MAX_Y][MAX_X]){
+    int i;
+    for(i=0;i<number_of_treasures;i++)
+        if((*map)[treasure_db[i].y][treasure_db[i].x]!='*') treasure_db[i].exist=0;
+	return;
 }
