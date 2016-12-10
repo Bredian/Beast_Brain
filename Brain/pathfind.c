8044c6c8b66ecefe9773605b157d78835b1cdbe6
fedosgad <fedosgad@gmail.com>
2016-12-10 23:49:15 +0300

    to make Bredian's merge

--- Brain/pathfind.c
+++ Brain/pathfind.c
@@ -1,8 +1,16 @@
+#include <tdlib.h>
 
 #include "brain.c"
 
 path trace(point from, char (*map)[max_y][max_x]) {
+	path to_ret;
 	
+	to_ret.len = (int)((*map)[from.y][from.x] - 'A');
+	to_ret.steps = (point*)malloc(to_ret.len*sizeof(point));
+	to_ret.steps[len - 1] = from;
+	
+	
+	return to_ret;
 }
 
 path trace_with_monsters(point from, char (*monster_map)[max_y][max_x], char (*map)[max_y][max_x]) {
