a6ba2efeb1884a613a24926f4b84b4f382e764e3
Bredian <sasha.kry@gmail.com>
2016-12-10 19:58:33 +0300

    Update scan_for_treasures.c

--- Brain/scan_for_treasures.c
+++ Brain/scan_for_treasures.c
@@ -1,10 +1,10 @@
 #include <brain.h>
-void scan_for_treasures(treasure* treasure_db, char (*map)[MAP_Y][MAP_X]){
+void scan_for_treasures(treasure* treasure_db, char (*map)[MAX_Y][MAX_X]){
     int i,j;
     number_of_treasures=0;
     for(i=0;i<MAX_Y;i++){
         for(j=0;j<MAX_X;j++){
-            if(map[i][j]=='*'){
+            if((*map)[i][j]=='*'){
                 number_of_treasures++;
                 treasure_db=realloc(treasure_db,number_of_treasures*sizeof(treasure));
                 treasure_db->x=j;
